33037ac71c7434434396b3f7eb65ab07a8525ebf
Maojiong.Fu <792657655@qq.com>
2017-04-24 10:20:38 +0800

    Demo Info

---
+++ WebDemo/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using MJ.BLL;
+using MJ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            OptUserInfo us = new OptUserInfo();
+            UserInfo result = us.GetModel(1);
+            return View(result);
+        }
+
+        public JsonResult Get()
+        {
+            return Json("");
+        }
+
+    }
+}
